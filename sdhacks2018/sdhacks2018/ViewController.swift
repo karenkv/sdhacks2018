0adc279ac08f974025b30bd65c2adc217df1cad5
rebeckur <leungrebecca16@gmail.com>
2018-10-13 07:50:12 -0700

    Adding empty Xcode project files

---
+++ sdhacks2018/sdhacks2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sdhacks2018
+//
+//  Created by Rebecca Leung on 10/13/18.
+//  Copyright © 2018 best team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
