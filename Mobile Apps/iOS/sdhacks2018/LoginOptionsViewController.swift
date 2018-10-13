ccedce08cea45779d7124e08139256a33e3f1b1c
rebeckur <leungrebecca16@gmail.com>
2018-10-13 15:32:59 -0700

    Adding login skeleton with email and snapchat

---
+++ Mobile Apps/iOS/sdhacks2018/LoginOptionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginOptionsViewController.swift
+//  sdhacks2018
+//
+//  Created by Rebecca Leung on 10/13/18.
+//  Copyright © 2018 best team. All rights reserved.
+//
+
+import UIKit
+
+class LoginOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Mobile Apps/iOS/sdhacks2018/SnapchatLoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SnapchatLoginViewController.swift
+//  sdhacks2018
+//
+//  Created by Rebecca Leung on 10/13/18.
+//  Copyright © 2018 best team. All rights reserved.
+//
+
+import UIKit
+
+class SnapchatLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
